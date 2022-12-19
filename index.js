df2a536a7e99f298ca72fe07c63e67d698a652c7
Iris <iridian95@live.com>
2022-12-18 19:59:09 -0500

    adding questions

---
+++ index.js
@@ -0,0 +1,69 @@
+// TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer")
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the name of your file?',
+        name: 'fileName',
+    },
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What is the description of your project?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'How did you install your project?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'How to use your project?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Do you want users to contribute to your project?',
+        name: 'contribution',
+    },
+    {
+        type: 'input',
+        message: 'What test have you run in your application?',
+        name: 'test',
+    },
+    {
+        type: 'input',
+        message: 'Enter your email.',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'Enter your GitHub',
+        name: 'github',
+    },
+    {
+        type: 'list',
+        message: 'Choose a license for your project.',
+        name: 'license',
+        choices: ['Apache2.0', 'BSD', 'MIT', 'None']
+    },
+];
+// TODO: Create a function to write README file
+function writeFile(fileName, data) {
+
+};
+// TODO: Create a function to initialize app
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((response) => {
+            fs.writeFileSync(`${response.fileName}.md`, generateMarkdown(response));
+        })
+}
